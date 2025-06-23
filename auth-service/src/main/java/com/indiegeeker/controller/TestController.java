8547e0e672a445f4618a50546141c4ed6311738b
indiegeeker <indiegeeker@gmail.com>
2025-06-23 12:11:55 +0800

    build: nacos

---
+++ auth-service/src/main/java/com/indiegeeker/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.indiegeeker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Desc:
+ * Author: wen
+ * Date: 2025/6/22
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping("hello")
+    public Object hello(){
+        return "hello";
+    }
+}
