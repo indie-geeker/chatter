6b1c995976d4ca5688c7f60624ad8688e3ecdfb5
indiegeeker <indiegeeker@gmail.com>
2025-06-23 21:59:20 +0800

    build: gateway

--- auth-service/src/main/java/com/indiegeeker/controller/TestController.java
+++ auth-service/src/main/java/com/indiegeeker/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.indiegeeker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 2025/6/22
  **/
 @RestController
+@RequestMapping("auth")
 public class TestController {
 
     @GetMapping("hello")
     public Object hello(){
-        return "hello";
+        return "hello auth";
     }
 }
--- file-service/src/main/java/indiegeeker/controller/TestController.java
+++ file-service/src/main/java/indiegeeker/controller/TestController.java
@@ -1,6 +1,7 @@
 package indiegeeker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 2025/6/22
  **/
 @RestController
+@RequestMapping("file")
 public class TestController {
 
     @GetMapping("hello")
     public Object hello(){
-        return "hello";
+        return "hello file";
     }
 }
