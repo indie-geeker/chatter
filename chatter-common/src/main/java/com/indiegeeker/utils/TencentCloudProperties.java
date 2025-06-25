9947e2ba658ccca2c24ec88049111e7e66ac4966
indiegeeker <indiegeeker@gmail.com>
2025-06-25 16:31:23 +0800

    feat: tencent sms utils

---
+++ chatter-common/src/main/java/com/indiegeeker/utils/TencentCloudProperties.java
@@ -0,0 +1,24 @@
+package com.indiegeeker.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Desc:
+ * Author: wen
+ * Date: 2025/6/25
+ **/
+@Component
+@Data
+@PropertySource("classpath:tencent-cloud.properties")
+@ConfigurationProperties(prefix = "tencent.cloud")
+public class TencentCloudProperties {
+    private String SecretId;
+    private String SecretKey;
+    private String SdkAppId;
+    private String SignName;
+    private String TemplateId;
+    private String[] TemplateParamSet;
+}
